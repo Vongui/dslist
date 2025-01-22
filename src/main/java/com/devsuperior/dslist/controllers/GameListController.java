ecc86c7b84e7bc1c358db55d6b9a12e3b78fcfe1
Guilherme Moraes <67127485+Vongui@users.noreply.github.com>
2025-01-22 12:38:46 -0300

    feat: implement endpoint searchByList with projection

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,21 @@ public class GameListController {
 
     @Autowired
     private GameListService gameListService;
+    @Autowired
+    private GameService gameService;
 
     @GetMapping
     public List<GameListDTO> findGameList(){
         List<GameListDTO> result = gameListService.findGameList();
         return result;
     }
+
+    //Por o endpoint ser localizado em "/lists" eh necessário que fica alocado no GamelistController, ao inves de ficar
+    //no GameController
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return result;
+    }
+
 }
